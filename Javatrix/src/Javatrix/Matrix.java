1bcd7c5df6f7e34ab495754124a0b90e77f03d48
joycemj <joycemj@CS-ABH309-14.appd.appstate.edu>
2016-02-05 11:39:39 -0500

    Added Constructor Stubs

--- Javatrix/src/Javatrix/Matrix.java
+++ Javatrix/src/Javatrix/Matrix.java
@@ -1,5 +1,32 @@
 package Javatrix;
 
-public class Matrix {
+public class Matrix 
+{
+
+	public Matrix(double[][] A)
+	{
+		
+	}
+	
+	public Matrix(double[][] A, int m, int n)
+	{
+		
+	}
+	
+	public Matrix(double[] vals, int m)
+	{
+		
+	}
+	
+	public Matrix(int m, int n)
+	{
+		
+	}
+	
+	public Matrix(int m, int n, double s)
+	{
+		
+	}
+	
 
 }
